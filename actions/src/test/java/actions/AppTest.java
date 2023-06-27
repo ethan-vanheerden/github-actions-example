9df42449e4e0c54b735968cc166aeef454d4613e
Ethan van Heerden <ethanvanheerden@Ethans-MacBook-Pro-2.local>
2023-06-27 13:00:58 -0400

    break

--- actions/src/test/java/actions/AppTest.java
+++ actions/src/test/java/actions/AppTest.java
@@ -10,7 +10,7 @@ import org.junit.Test;
 public class AppTest {
 	@Test
 	public void testGiveMeNumber() {
-		assertEquals(Utils.giveMeNumber(-1), -1);
+		assertEquals(Utils.giveMeNumber(1), -1);
 		assertEquals(Utils.giveMeNumber(0), 0);
 		assertEquals(Utils.giveMeNumber(5), 5);
 	}
