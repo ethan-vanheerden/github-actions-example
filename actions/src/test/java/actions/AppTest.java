63c6e3bf67332e4f8ab133f2d90f1dd81e39d23f
Ethan van Heerden <ethanvanheerden@Ethans-MacBook-Pro-2.local>
2023-06-27 12:18:30 -0400

    setup

---
+++ actions/src/test/java/actions/AppTest.java
@@ -0,0 +1,17 @@
+package actions;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	@Test
+	public void testGiveMeNumber() {
+		assertEquals(Utils.giveMeNumber(-1), -1);
+		assertEquals(Utils.giveMeNumber(0), 0);
+		assertEquals(Utils.giveMeNumber(5), 5);
+	}
+}
